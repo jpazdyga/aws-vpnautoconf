10600cfc217e662b4f5aed2671277ac6d40fc028
Jakub.Pazdyga <Jakub.Pazdyga@ft.com>
2015-08-21 11:22:47 +0100

    Added some customization options

--- create_vpn_connection.py
+++ create_vpn_connection.py
@@ -2,24 +2,29 @@
 
 import boto
 from boto.vpc import VPCConnection
+outnetcidr="10.20.30.0/24"
+cgw="cgw-a3dfeed7"
+vgw="vgw-6a0b3a1e"
 
 def get():
- vpnid = vpns[0].id
- state = vpns[0].state
- tunnels = vpns[0].tunnels
+ for i in vpns:
+ print(i)
+  vpnid = i.id
+  state = i.state
+  tunnels = i.tunnels
  if state != "deleted":
   print (vpnid, state, tunnels);
   if routeflag == "1":
-   addroute = c.create_vpn_connection_route("10.10.10.0/24", vpnid)
+   addroute = c.create_vpn_connection_route(outnetcidr, vpnid)
    print addroute;
    exit()
   else:
    exit()
  else:
-  print ("Deleted vpns: ", vpnid);
+  print ("Deleted vpn: ", vpnid);
 
 def createvpn():
- newvpn = c.create_vpn_connection("ipsec.1","cgw-a3dfeed7","vgw-6a0b3a1e",1,0)
+ newvpn = c.create_vpn_connection("ipsec.1",cgw,vgw,1,0)
  print newvpn;
  routeflag="1"
  get()
